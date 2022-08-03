9f497b2c6201ff2d8489fd9e7e40441731dc5d9c
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-03 13:34:23 +0300

    user login system
    internship

---
+++ UserLogin/UserLogin/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string userName = "lunanera";
+            int password = 16111999;
+            while (true)
+            {
+                Console.WriteLine("Please enter your user name and password for login...");
+
+                Console.Write("User name: ");
+                string enteredUserName = Console.ReadLine();
+
+                Console.Write("Password: ");
+                int enteredpassword = 0;
+                
+                int.TryParse(Console.ReadLine(), out enteredpassword);
+
+                if (enteredUserName == userName)
+                {
+                    Console.WriteLine("Your user name is correct");
+                    if (enteredpassword == password)
+                    {
+                        Console.WriteLine("Login successful...");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your user name is correct but your password is wrong...");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Your user name is wrong...");
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
